a59bbef3cb4b31704fc1f228c617c90351585b9b
fatmacafri <fatmacafri.1@gmail.com>
2021-02-22 14:54:13 +0300

    Generic Repository and EF added.

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -7,6 +7,8 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll();
+        List<Product> GetAll(); //Tümünü getir.
+        List<Product> GetAllByCategoryId(int id); //CategoryId'sine göre getir.
+        List<Product> GetByUnitPrice(decimal min, decimal max);
     }
 }
